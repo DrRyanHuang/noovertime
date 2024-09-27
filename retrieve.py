81656fd5252e7af45da9373240f6230336480aa8
Ryan <zihaohuang@aliyun.com>
2024-09-27 21:10:58 +0800

    🤡 fix baseline same cls_embedding

--- retrieve.py
+++ retrieve.py
@@ -13,15 +13,16 @@ def get_topk(q, a, k):
         q_inputs = tokenizer(text=q, max_seq_len=512, padding="max_length")
         query_input_ids = paddle.to_tensor([q_inputs["input_ids"]])
         query_token_type_ids = paddle.to_tensor([q_inputs["token_type_ids"]])
-        _, cls_embedding = model(query_input_ids, query_token_type_ids)
+        _, cls_embedding_query = model(query_input_ids, query_token_type_ids)
+        cls_embedding_query = cls_embedding_query.squeeze()
 
         a_inputs = tokenizer(text=a, max_seq_len=512, padding="max_length")
         answer_input_ids = paddle.to_tensor(a_inputs["input_ids"])
         answer_token_type_ids = paddle.to_tensor(a_inputs["token_type_ids"])
-        _, cls_embedding = model(answer_input_ids, answer_token_type_ids)
+        _, cls_embedding_tool = model(answer_input_ids, answer_token_type_ids)
 
-        cos_sim = paddle.dot(cls_embedding, cls_embedding) / (
-            paddle.linalg.norm(cls_embedding) * paddle.linalg.norm(cls_embedding)
+        cos_sim = (cls_embedding_query * cls_embedding_tool).sum(-1) / (
+            paddle.linalg.norm(cls_embedding_query) * paddle.linalg.norm(cls_embedding_tool, axis=-1)
         )
 
     values, indices = paddle.topk(cos_sim, k=k)
